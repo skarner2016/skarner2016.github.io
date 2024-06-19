87ce2ec50b99c2b68f65058de94b357fd5c0942c
skarner2016 <skarner2016@gmail.com>
2024-06-19 09:35:55 +0800

    test:百度统计

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -44,23 +44,19 @@ export default defineConfig({
       ],
       
       // 百度统计代码
-      ['script', 
-      {},
-      `
-        var _hmt = _hmt || [];
+      [
+        'script',
+        {},
+        `
+        window._hmt = window._hmt || [];
         (function() {
           var hm = document.createElement("script");
           hm.src = "https://hm.baidu.com/hm.js?f43715192be90c17a095ac8388770b67";
           var s = document.getElementsByTagName("script")[0]; 
           s.parentNode.insertBefore(hm, s);
         })();
-        (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "jud16drf49");
-      `
-      ]
+        `,
+      ],
     ],
     footer: {
       message: 'Powered by <a href="https://vitepress.dev/">Vitepress</a>.',
